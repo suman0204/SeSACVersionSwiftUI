a6d1c36b1ea3ce2727441a5cc2f261f24e8579a7
sumanhong <suman0204@naver.com>
2023-11-23 10:11:53 +0900

    Initial Commit

---
+++ SeSACVersionSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SeSACVersionSwiftUI
+//
+//  Created by 홍수만 on 2023/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SeSACVersionSwiftUI/SeSACVersionSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeSACVersionSwiftUIApp.swift
+//  SeSACVersionSwiftUI
+//
+//  Created by 홍수만 on 2023/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SeSACVersionSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
